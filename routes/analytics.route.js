1f05d727eb8306189b185d0b34917003e4c72706
mallik-amaan <maman.bscs22seecs@seecs.edu.pk>
2026-03-04 09:49:46 +0500

    new commit

---
+++ routes/analytics.route.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const express = require('express');
+const supabase = require('../config/supabase.config');
+const { type } = require('os');
+
+const router = express.Router();
+
+
+router.post('/sumbit-review',(req,res) => {
+    try{
+        const {passed,flagged} = req.body;
+        console.log(`passed: ${passed}`)
+        console.log(`flagged: ${flagged}`)
+
+        
+    }
+    catch(error){
+
+    }
+    finally{
+
+    }
+})
